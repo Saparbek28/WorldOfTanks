03750c44f3a0b3d72a6a76c1cbf082112a280548
Байназар Сапарбек Даулетулы <@corp.itstep>
2019-03-19 15:35:02 +0600

    adding files

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldOfTAnks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tank tank = new tank();
+            tank[] Win = new tank[7];
+            tank[] TankT34 = new tank[7];
+            tank[] TankPantera = new tank[7];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < TankT34.Length; i++)
+            {
+                Win[i] = new tank();
+            }
+            for (int i = 0; i < TankT34.Length; i++)
+            {
+                TankT34[i] = new tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "T34");
+            }
+
+            for (int i = 0; i < TankT34.Length; i++)
+            {
+                TankPantera[i] = new tank(rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100), "Pantera");
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                TankT34[i].Print();
+                TankPantera[i].Print();
+                Win[i] = TankT34[i] * TankPantera[i];
+                Console.WriteLine();
+                Console.WriteLine($"В {i + 1} схватке победил");
+
+                Win[i].Print();
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Tank.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldOfTAnks
+{
+    public class tank
+    {
+        protected string Name;
+        protected int Ammunition;
+        protected int LvlArmor;
+        protected int LvlManeuver;
+        public tank(int Ammunition, int LvlArmor, int LvlManeuver, string Name)
+        {
+
+            this.Ammunition = Ammunition;
+            this.LvlArmor = LvlArmor;
+            this.LvlManeuver = LvlManeuver;
+            this.Name = Name;
+        }
+        public tank()
+        {
+            Ammunition = 0;
+            LvlArmor = 0;
+            LvlManeuver = 0;
+            Name = "null";
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name = " + Name);
+            Console.WriteLine("Ammunition = " + Ammunition + " LvlArmor = " + LvlArmor + " LvlManeuver = " + LvlManeuver);
+        }
+
+        public static tank operator *(tank T34, tank pantera)
+        {
+            if (T34.Ammunition > pantera.Ammunition && T34.LvlArmor > pantera.LvlArmor)
+            {
+                return T34;
+            }
+            if (T34.Ammunition > pantera.Ammunition && T34.LvlManeuver > pantera.LvlManeuver)
+            {
+                return T34;
+            }
+            if (T34.LvlArmor > pantera.LvlArmor && T34.LvlManeuver > pantera.LvlManeuver)
+            {
+                return T34;
+            }
+            else
+            {
+                return pantera;
+            }
+        }
+    }
+}
